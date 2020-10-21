506385ca4283a543ae61bec5d2f04e5bfa766859
Peter Kim <peter@example.com>
2020-10-21 12:55:08 +1100

    another commit

--- models/soynet-activity-model.js
+++ models/soynet-activity-model.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
+require('mongoose-double')(mongoose);
 const Schema = mongoose.Schema
+const SchemaTypes = mongoose.Schema.Types;
 
 const Activity = new Schema(
     {
         faceid: { type: Number, required: true },
         name: { type: String, required: true },
         datetime: { type: Date, required: true },
-        deviceid: { type: Number },
-        feature: { type: Number },
+        deviceid: { type: Number, required: true  },
+        //feature: { type: SchemaTypes.Double, required: true  },
+        feature: { type: [SchemaTypes.Double], required: true  },
         visitcnt: { type: Number},
         stayhour: { type: Number},
         temperature: { type: Number},
